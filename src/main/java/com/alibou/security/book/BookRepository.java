16885b69655121dc9b9b726712d7f18fa87c4791
FerchichiMohamedMehdi <mohamedmehdi.ferchichi@etudiant-isi.utm.tn>
2024-01-20 23:55:35 +0100

    repo

--- src/main/java/com/alibou/security/book/BookRepository.java
+++ src/main/java/com/alibou/security/book/BookRepository.java
@@ -2,5 +2,7 @@ package com.alibou.security.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Book;
+
 public interface BookRepository extends JpaRepository<Book, Integer> {
 }
